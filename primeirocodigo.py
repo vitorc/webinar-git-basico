d023294cd1f770e77c1d1d19385c64eceb888a28
vitorcardoso_noesis <vitor.c.santos@noesis.com.br>
2020-06-18 09:30:30 -0300

    Melhorando retorno da function codigobasico e incluind utf-8 para correção de erro ASCII

--- primeirocodigo.py
+++ primeirocodigo.py
@@ -1,6 +1,7 @@
+# -*- coding: utf-8 -*-
 def codigobasico():
     valor1 = 800
     valor2 = 100
     soma = valor1+valor2
-    print("Aqui esta o retorno de uma soma:" + str(soma))
+    print("O  valor 1 é: " + str(valor1) + ", O valor 2 é: " + str(valor2) + ", Aqui esta o retorno da soma: " + str(soma))
 codigobasico()
