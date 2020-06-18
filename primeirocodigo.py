9c60ddc3af6c8f808a8fcfd66c22d8b516f040b2
Larissa Ferreira <larissafer_@live.com>
2020-06-18 14:34:20 -0300

    TESTE: Alteração de valores

--- primeirocodigo.py
+++ primeirocodigo.py
@@ -1,6 +1,6 @@
 def codigobasico():
-    valor1 = 80
-    valor2 = 100
+    valor1 = 90
+    valor2 = 90
     soma = valor1+valor2
     print("Aqui esta o retorno de uma soma:" + str(soma))
 print (codigobasico())
