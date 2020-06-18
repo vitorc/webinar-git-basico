51b1a1428c22fd4a81e11b8b87e2522e19eb54ee
vitorcardoso_noesis <vitor.c.santos@noesis.com.br>
2020-06-18 11:03:25 -0300

    Corrigindo texto do print

--- primeirocodigo.py
+++ primeirocodigo.py
@@ -3,5 +3,5 @@ def codigobasico():
     valor1 = 800
     valor2 = 100
     soma = valor1+valor2
-    print("O  valor 1 é: " + str(valor1) + ", O valor 2 é: " + str(valor2) + ", Aqui esta o retorno da soma: " + str(soma))
+    print("O  valor 1 é: " + str(valor1) + ", O valor 2 é: " + str(valor2) + ", Aqui está o retorno da soma: " + str(soma))
 codigobasico()
