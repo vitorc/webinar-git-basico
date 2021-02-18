2951bc29e3d935e09d04e69e5173e4e2569a47db
Claudio <claudio.r.castro@noesis.pt>
2021-02-18 11:24:18 -0300

    alteracao de valores e textos

--- primeirocodigo.py
+++ primeirocodigo.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 def codigobasico():
-    valor1 = 800
-    valor2 = 100
+    valor1 = 500
+    valor2 = 50
     soma = valor1+valor2
-    print("O  valor 1 é: " + str(valor1) + ", O valor 2 é: " + str(valor2) + ", Aqui está o retorno da soma: " + str(soma))
+    print("O  valor 1 é: " + str(valor1) + ", O valor 2 é: " + str(valor2) + ", Aqui está o RESULTADO da soma: " + str(soma))
 codigobasico()
 
