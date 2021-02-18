3e4d4534a10f225fdb3e8d7028c07c5ed2d79d9b
unknown <issias.p.castro@noesis.pt>
2021-02-18 12:05:42 -0300

    Alterado os arquivos

--- primeirocodigo.py
+++ primeirocodigo.py
@@ -1,8 +1,17 @@
 # -*- coding: utf-8 -*-
 def codigobasico():
-    valor1 = 800
-    valor2 = 100
+    valor1 = 600
+    valor2 = 200
     soma = valor1+valor2
+    subtracao = valor1 - valor2
+    divisao = valor1 / valor2
+    multiplicacao = valor1*valor2
     print("O  valor 1 é: " + str(valor1) + ", O valor 2 é: " + str(valor2) + ", Aqui está o retorno da soma: " + str(soma))
+codigobasico()
+    print("O  valor 1 é: " + str(valor1) + ", O valor 2 é: " + str(valor2) + ", Aqui está o retorno da subtracao: " + str(soma))
+codigobasico()
+    print("O  valor 1 é: " + str(valor1) + ", O valor 2 é: " + str(valor2) + ", Aqui está o retorno da divisao: " + str(soma))
+codigobasico()
+    print("O  valor 1 é: " + str(valor1) + ", O valor 2 é: " + str(valor2) + ", Aqui está o retorno da multiplicacao: " + str(soma))
 codigobasico()
 
