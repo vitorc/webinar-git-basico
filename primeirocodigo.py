5745517b62a0c63734e2c2fa9e06a18f15fc7b0b
brunavianna88 <brunavianna88.com>
2021-03-30 15:48:40 -0300

    alteraçao feita por bruna no treinamento

--- primeirocodigo.py
+++ primeirocodigo.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 def codigobasico():
-    valor1 = 800
-    valor2 = 100
+    valor1 = 200
+    valor2 = 200
     soma = valor1+valor2
     print("O  valor 1 é: " + str(valor1) + ", O valor 2 é: " + str(valor2) + ", Aqui está o retorno da soma: " + str(soma))
 codigobasico()
