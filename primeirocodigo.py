1f13790cc0031fd73b59c541e432b29cbf61ef08
raquelcassia <rcassia_scarvalho@hotmail.com>
2021-03-30 15:48:31 -0300

    alterando valores do arquivo de treinamento

--- primeirocodigo.py
+++ primeirocodigo.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 def codigobasico():
-    valor1 = 800
-    valor2 = 100
+    valor1 = 550
+    valor2 = 150
     soma = valor1+valor2
     print("O  valor 1 é: " + str(valor1) + ", O valor 2 é: " + str(valor2) + ", Aqui está o retorno da soma: " + str(soma))
 codigobasico()
