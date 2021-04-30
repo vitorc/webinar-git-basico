596ae198df32ee81ee42c028b70973691b4d6343
HugoSS1986 <hugo.s.silva@noesis.pt>
2021-04-30 16:50:57 -0300

    Alterando arquivo primeirocodigo.py

--- primeirocodigo.py
+++ primeirocodigo.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 def codigobasico():
-    valor1 = 800
-    valor2 = 100
+    valor1 = 123
+    valor2 = 456
     soma = valor1+valor2
-    print("O  valor 1 é: " + str(valor1) + ", O valor 2 é: " + str(valor2) + ", Aqui está o retorno da soma: " + str(soma))
+    print("O  valor 1 é: " + str(valor1) + ", O valor 2 é: " + str(valor2) + ", Aqui está o retorno da soma feito por Hugo SS: " + str(soma))
 codigobasico()
 
